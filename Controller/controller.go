587827405ff9152543ccd985b92a115db7088d4f
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2025-11-26 19:17:00 +0530

    code: created functions to create service

--- Controller/controller.go
+++ Controller/controller.go
@@ -1,6 +1,15 @@
 package Controller
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	network "k8s.io/kubernetes/pkg/apis/networking"
+	"k8s.io/apimachinery/pkg/util/wait"
+	appinformer "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	applisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
@@ -8,12 +17,84 @@ import (
 )
 
 type controller struct {
-	clientset kubernetes.Interface
-	lister applisters.DeploymentLister
-	depCacheSyncd cache.InformerSynced
-	queue  workqueue.RateLimitingInterface
+
+	clientset     kubernetes.Interface
+	lister        applisters.DeploymentLister
+	depCacheSyncd cache.InformerSynced            // to check if the cache has been synced or not
+	queue         workqueue.RateLimitingInterface // for pushing events
 }
 
-func New() *controller{
-	return &controller{}
+func New(clientset kubernetes.Clientset, depInformer appinformer.DeploymentInformer) *controller {
+	c:= &controller{
+		clientset:     &clientset,
+		lister:        depInformer.Lister(),
+		depCacheSyncd: depInformer.Informer().HasSynced,
+		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "netsek"),
+	}
+	depInformer.Informer().AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDelete,
+		},
+	)
+	return c
+}
+
+// This runs the netsek controller
+func (c *controller) Run(ch <-chan struct{}) {
+	fmt.Println("Controller Starting")
+	if !cache.WaitForCacheSync(ch, c.depCacheSyncd) { // since the informer maintains a local cahce, we will have to wait for that cache to be synced or inialized if it is the first time
+		fmt.Println("Error watiting for cache to be synced")
+	}
+	go wait.Until(c.worker, 1*time.Second, ch) // this calls a specific function for a duration until the channel is closed
+	<-ch                                       // waits for something to be recieved but since nothing will the go routine above will not return
+}
+
+func (c *controller) worker() {
+	for c.processItem() {
+
+	}
+}
+func (c *controller) processItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown{
+		return false
+	}
+
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err!=nil{
+		fmt.Println("Error getting key from cache:", err)
+	}
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err!=nil{
+		fmt.Println("Error getting namespace, name from key:", err)
+	}
+	err = c.syncDeployment() // things we want to once the deployment is created
+	if err!=nil{
+		//re try
+		fmt.Println("Error syncing deploymemt:", err)
+		return false
+	}
+
+	return true
+}
+func (c *controller) syncDeployment(ns,name string) error{
+	// create network policy
+	//np:= network.NetworkPolicy{}
+
+
+	//create service
+	svc := corev1.Service{}
+	_, err := c.clientset.CoreV1().Services(ns).Create(context.Background(),&svc,metav1.CreateOptions{})
+	if err!=nil{
+		fmt.Println("creating service error:", err)
+	} 
+	return nil
+}
+
+func (c *controller) handleAdd(obj interface{}) {
+	c.queue.Add(obj)
+}
+func (c *controller) handleDelete(obj interface{}) {
+	c.queue.Add(obj)
 }
--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"fmt"
 	"log"
 	"time"
-
+	"rghdrizzle/netsek/Controller"
 	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
+	//"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
+	//"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -30,25 +30,10 @@ func main(){
   if err!=nil{
     log.Fatal("Error while creating clientset:",err)
   }
+  ch := make(chan struct{}) // recieve only channel
   informerfactory := informers.NewSharedInformerFactory(clientset,30*time.Second)
-
-  podinformer := informerfactory.Core().V1().Pods()
-
-  podinformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-	AddFunc: func(new interface{}){
-		fmt.Println("add was called")
-	},
-	UpdateFunc: func(old,new interface{}){
-		fmt.Println("update was called")
-	},
-	DeleteFunc: func(obj interface{}){
-		fmt.Println("delete was called")
-	},
-
-  })
-  informerfactory.Start(wait.NeverStop) // when the cahce is initialized
-  informerfactory.WaitForCacheSync(wait.NeverStop)
-  pod, err := podinformer.Lister().Pods("default").Get("default") // The informer has a lister through which we can list. This call goes to the informer and gets a response from the cache instead of the api server
-  fmt.Println(pod)
+  informerfactory.Start(ch)
+  cont := Controller.New(*clientset,informerfactory.Apps().V1().Deployments())
+  cont.Run(ch)
 
 }
